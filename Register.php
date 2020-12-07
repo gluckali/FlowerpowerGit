2d4b58640709de4a0292fd3858c2addc2dd9ae48
Omar <omarelmedny17@gmail.com>
2020-12-07 13:30:07 +0100

    Github Flower
    Forms + ERD

---
+++ Register.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <form action="action_page.php">
+    <div class="container">
+      <h1>Medewerker registeren</h1>
+      <p>haal je medewerkerscode na het registeren bij Tom holen</p>
+      <hr>
+
+      <label for="email"><b>Email</b></label>
+      <input type="text" placeholder="Enter Email" name="email" id="email" required>
+
+          <label for="voornaam"><b>Voornaam</b></label>
+      <input type="voornaam" placeholder="voornaam" name="voornaam" id="voornaam" required>
+
+
+          <label for="Achternaam"><b>Achternaam</b></label>
+      <input type="text" placeholder="Achternaam" name="Achternaam" id="Achternaam" required>
+
+
+          <label for="gebruikersnaam"><b>gebruikersnaam</b></label>
+      <input type="gebruikersnaam" placeholder="gebruikersnaam" name="gebruikersnaam" id="gebruikersnaam" required>
+
+
+      <label for="psw"><b>Wachtwoord</b></label>
+      <input type="password" placeholder="Wachtwoord" name="psw" id="psw" required>
+
+      <label for="psw-repeat"><b>Wachtwoord nogmaals</b></label>
+      <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required>
+      <hr>
+      <button type="submit" class="registerbtn">Register</button>
+  </div>
+    </div>
+
+    <div class="container signin">
+      <p>Already have an account? <a href="login.php">Sign in</a>.</p>
+    </div>
+  </form>
+</html>
